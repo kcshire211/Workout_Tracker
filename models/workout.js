b857fe72de497f104b1fb0b04f6b785b3b57a9a7
Korey Cheshire <korey.cheshire@gmail.com>
2021-12-12 13:06:47 -0800

    working on models

---
+++ models/workout.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: Date.now
+        },
+        exercices: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: "What type of exercise?"
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "What is the name of this exercise?"
+                },
+                duration: {
+                    type: Number,
+                    required: "How many minutes is this exercise?"
+                },
+                weight: {
+                    type: Number
+                },
+                reps: {
+                    type: Number
+                },
+                sets: {
+                    type: Number
+                },
+                distance: {
+                    type: Number
+                }
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virticles: true
+        }
+    }
+);
